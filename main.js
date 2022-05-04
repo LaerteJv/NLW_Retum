862efebcf53d21a19938b495f18359ea0bd16c5c
LaerteJv <laertejosevieira66@gmail.com>
2022-05-04 17:28:28 -0300

    2° comite aula 2

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 function onScroll() {
-  console.log(scrollY == 0)
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
   }
 }
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
 
 
 
