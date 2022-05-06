b7f5cecc843abdfcfd1df7e4f25e6215cdabd299
LaerteJv <laertejosevieira66@gmail.com>
2022-05-06 20:18:54 -0300

    Final aula 3

--- main.js
+++ main.js
@@ -13,14 +13,17 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
-  
-  
-  
 
-
-
-
-
-
-
-
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services, 
+#services header,
+#services .cards
+#about header,
+#about .content`)
